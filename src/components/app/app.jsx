98a090a32c2d91743d439b6182766cbdc8ad16f9
Lev <salamatinlefff@gmail.com>
2022-07-06 11:42:07 +0400

    refactor

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -15,12 +15,12 @@ function App() {
   return (
     <Layout className="main">
       <Tabs defaultActiveKey="1" centered size="large">
-        <TabPane className="container" tab="Search" key="1">
+        <TabPane className="content-container" tab="Search" key="1">
           <Input placeholder="Type to search..." allowClear onChange={onChange} />
           <MoviesList url="https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp" />
         </TabPane>
 
-        <TabPane className="container" tab="Rated" key="2">
+        <TabPane className="content-container" tab="Rated" key="2">
           <Input placeholder="Type to search among rated..." allowClear onChange={onChange} />
           <MoviesList url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZ2fSqwjlzwi465PLW856Urwisp0ua7axWHQ&usqp=CAU" />
         </TabPane>
